d361c75112be9e54852f2ffce27f013709b22c18
Atharv1151 <atharv_b220740ee@nitc.ac.in>
2025-07-10 00:54:43 +0530

    Fix Corrected Import for Render

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import Auth from './Routes/UserRoutes.js';
+import Auth from './routes/UserRoutes.js';
 import codingprofiles from './routes/codingprofiles.js';
 
 
